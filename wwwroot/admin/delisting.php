ed82ceb4588e243f6cc4311c70c6026fbd2fdb89
Ragowit <markus.persson@grh.se>
2020-01-07 21:12:28 +0100

    Fixed title for admin delisting game.

--- wwwroot/admin/delisting.php
+++ wwwroot/admin/delisting.php
@@ -30,7 +30,7 @@ if (ctype_digit(strval($_POST["game"]))) {
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Admin ~ Cheater</title>
+        <title>Admin ~ Delisted Game</title>
     </head>
     <body>
         <form method="post">
