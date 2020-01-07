f676d14a66374cbcde9010af00c4a8473c7d1698
Ragowit <markus.persson@grh.se>
2020-01-07 22:35:57 +0100

    1.1.0

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -8,7 +8,7 @@
                     <a href="https://github.com/Ragowit/psn100/issues">Issues</a>
                 </div>
                 <div class="col-md-4 text-right">
-                    v1.0.2
+                    v1.1.0
                 </div>
             </div>
         </footer>
