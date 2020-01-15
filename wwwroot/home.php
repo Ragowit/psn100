62796dae0212a15727242a281534a4427ebd77cc
Ragowit <markus.persson@grh.se>
2020-01-15 20:48:57 +0100

    Home Removed the heading. Not needed since the title is always in the menu.

--- wwwroot/home.php
+++ wwwroot/home.php
@@ -6,12 +6,6 @@ require_once("header.php");
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>PSN 100%</h1>
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="col-12">
                 <div class="alert alert-info" role="alert">
                     <?php
                     $query = $database->prepare("SELECT COUNT(*) FROM player_queue");
