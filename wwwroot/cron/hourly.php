0ecda875fd7b7f24fc2f4cc7445f27998a9ca71f
Ragowit <markus.persson@grh.se>
2020-01-26 21:18:13 +0100

    Cron - Hourly Fix trophy rarity calculation.

--- wwwroot/cron/hourly.php
+++ wwwroot/cron/hourly.php
@@ -54,13 +54,13 @@ $query = $database->prepare("UPDATE trophy_title tt SET tt.difficulty = CASE WHE
 $query->execute();
 
 // Recalculate trophy rarity. THIS ONE IS SLOW! TODO: How to speed it up?
-$query = $database->prepare("UPDATE trophy t SET t.rarity_percent = (
+$query = $database->prepare("UPDATE trophy t SET t.rarity_percent = IF((SELECT owners FROM trophy_title tt WHERE tt.np_communication_id = t.np_communication_id) = 0, 0, (
     SELECT COUNT(*) FROM trophy_earned te
     JOIN player p USING (account_id)
     WHERE te.np_communication_id = t.np_communication_id AND te.group_id = t.group_id AND te.order_id = t.order_id AND p.status = 0 AND p.rank <= 100000
     ) / (
-    SELECT IF(owners > 0, owners, 1) FROM trophy_title tt WHERE tt.np_communication_id = t.np_communication_id
-    ) * 100");
+    SELECT owners FROM trophy_title tt WHERE tt.np_communication_id = t.np_communication_id
+    ) * 100)");
 $query->execute();
 
 // Recalculate trophy rarity point
