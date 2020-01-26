f4e1af1de05f2384720ae090de565f2d9bfaeb1f
Ragowit <markus.persson@grh.se>
2020-01-26 18:21:22 +0100

    Cron - Weekly Removed comment because of new server, and fixed the require_once.

--- wwwroot/cron/weekly.php
+++ wwwroot/cron/weekly.php
@@ -2,8 +2,7 @@
 ini_set("max_execution_time", "0");
 ini_set("mysql.connect_timeout", "0");
 set_time_limit(0);
-// The lines above doesn't seem to do anything on my webhost. 504 after about 5 minutes anyway.
-require_once("../init.php");
+require_once("/home/psn100/public_html/init.php");
 
 $query = $database->prepare("UPDATE player SET rank_last_week = rank, rarity_rank_last_week = rarity_rank, rank_country_last_week = rank_country, rarity_rank_country_last_week = rarity_rank_country");
 $query->execute();
