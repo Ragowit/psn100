b8785f0498cac2934fd17cdbd6f39fa28d62ec3d
Ragowit <markus.persson@grh.se>
2020-01-29 20:10:50 +0100

    Add To Queue Prevent empty PSN names to be inserted into the queue.

--- wwwroot/add_to_queue.php
+++ wwwroot/add_to_queue.php
@@ -3,17 +3,21 @@ require_once("init.php");
 
 $player = $_REQUEST["q"];
 
-// Insert player into the queue
-//$query = $database->prepare("INSERT IGNORE INTO player_queue (online_id) VALUES (:online_id)");
-$query = $database->prepare("INSERT INTO player_queue (online_id) VALUES (:online_id) ON DUPLICATE KEY UPDATE request_time=NOW()"); // Currently our initial backlog is huge, so use this for a while.
-$query->bindParam(":online_id", $player, PDO::PARAM_STR);
-$query->execute();
+if (!isset($player) || trim($player) === "") {
+    echo "PSN name can't be empty.";
+} else {
+    // Insert player into the queue
+    //$query = $database->prepare("INSERT IGNORE INTO player_queue (online_id) VALUES (:online_id)");
+    $query = $database->prepare("INSERT INTO player_queue (online_id) VALUES (:online_id) ON DUPLICATE KEY UPDATE request_time=NOW()"); // Currently our initial backlog is huge, so use this for a while.
+    $query->bindParam(":online_id", $player, PDO::PARAM_STR);
+    $query->execute();
 
-// Check position
-$query = $database->prepare("SELECT id FROM (SELECT *, @rownum:=@rownum + 1 AS id FROM player_queue, (SELECT @rownum:=0) r ORDER BY request_time) d WHERE d.online_id = :online_id");
-$query->bindParam(":online_id", $player, PDO::PARAM_STR);
-$query->execute();
-$position = $query->fetchColumn();
+    // Check position
+    $query = $database->prepare("SELECT id FROM (SELECT *, @rownum:=@rownum + 1 AS id FROM player_queue, (SELECT @rownum:=0) r ORDER BY request_time) d WHERE d.online_id = :online_id");
+    $query->bindParam(":online_id", $player, PDO::PARAM_STR);
+    $query->execute();
+    $position = $query->fetchColumn();
 
-$player = htmlentities($player, ENT_QUOTES, "UTF-8");
-echo "<a href=\"/player/". $player ."\">". $player ."</a> is in the update queue, currently in position ". $position;
+    $player = htmlentities($player, ENT_QUOTES, "UTF-8");
+    echo "<a href=\"/player/". $player ."\">". $player ."</a> is in the update queue, currently in position ". $position;
+}
