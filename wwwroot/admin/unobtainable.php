4ee8eaa3bc746684febec48a3afb56f4e157857c
Ragowit <markus.persson@grh.se>
2020-02-01 14:43:29 +0100

    Admin Use NOW() instead of some kind made up date.

--- wwwroot/admin/unobtainable.php
+++ wwwroot/admin/unobtainable.php
@@ -147,7 +147,7 @@ if (ctype_digit(strval($_POST["trophy"]))) {
     $players->bindParam(":np_communication_id", $trophy["np_communication_id"], PDO::PARAM_STR);
     $players->execute();
     while ($player = $players->fetch()) {
-        $query = $database->prepare("INSERT INTO player_queue (online_id, request_time) VALUES (:online_id, '2000-01-01 00:00:00') ON DUPLICATE KEY UPDATE request_time='2000-01-01 00:00:00'"); // An early date like '2000-01-01 00:00:00' makes it first in queue
+        $query = $database->prepare("INSERT INTO player_queue (online_id) VALUES (:online_id) ON DUPLICATE KEY UPDATE request_time=NOW()");
         $query->bindParam(":online_id", $player["online_id"], PDO::PARAM_STR);
         $query->execute();
     }
