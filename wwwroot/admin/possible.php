5da4264461bcbccbfaa80c2a31c006ee0c596092
Ragowit <markus.persson@grh.se>
2020-02-22 22:58:42 +0100

    Admin - Possible Cheaters Added checks against Night Trap - 25th Anniversary Edition (PSVITA).

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -72,6 +72,11 @@ require_once("../init.php");
                 OR (te.np_communication_id = 'NPWR04361_00' AND te.group_id = 'default' AND te.order_id = 39)
                 OR (te.np_communication_id = 'NPWR04361_00' AND te.group_id = 'default' AND te.order_id = 40)
                 OR (te.np_communication_id = 'NPWR03434_00' AND te.group_id = 'default' AND te.order_id = 4)
+                OR (te.np_communication_id = 'NPWR14011_00' AND te.group_id = 'default' AND te.order_id = 0)
+                OR (te.np_communication_id = 'NPWR14011_00' AND te.group_id = 'default' AND te.order_id = 12)
+                OR (te.np_communication_id = 'NPWR14011_00' AND te.group_id = 'default' AND te.order_id = 23)
+                OR (te.np_communication_id = 'NPWR14011_00' AND te.group_id = 'default' AND te.order_id = 24)
+                OR (te.np_communication_id = 'NPWR14011_00' AND te.group_id = 'default' AND te.order_id = 25)
                 ) AND p.status = 0 GROUP BY online_id) x USING (np_communication_id)
             ORDER BY player_name");
         $query->execute();
