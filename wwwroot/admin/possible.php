8a4ded87934f1576bd80ad75567e8fbab2cb6fb6
Ragowit <markus.persson@grh.se>
2020-03-06 22:07:00 +0100

    Admin - Possible Cheaters Locks' Quest

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -177,6 +177,13 @@ require_once("../init.php");
                 OR (te.np_communication_id = 'NPWR11373_00' AND te.group_id = 'default' AND te.order_id = 9)
                 OR (te.np_communication_id = 'NPWR10806_00' AND te.group_id = 'default' AND te.order_id = 0)
                 OR (te.np_communication_id = 'NPWR10806_00' AND te.group_id = 'default' AND te.order_id = 23)
+                OR (te.np_communication_id = 'NPWR12464_00' AND te.group_id = 'default' AND te.order_id = 3)
+                OR (te.np_communication_id = 'NPWR12464_00' AND te.group_id = 'default' AND te.order_id = 4)
+                OR (te.np_communication_id = 'NPWR12464_00' AND te.group_id = 'default' AND te.order_id = 7)
+                OR (te.np_communication_id = 'NPWR12464_00' AND te.group_id = 'default' AND te.order_id = 8)
+                OR (te.np_communication_id = 'NPWR12464_00' AND te.group_id = 'default' AND te.order_id = 9)
+                OR (te.np_communication_id = 'NPWR12464_00' AND te.group_id = 'default' AND te.order_id = 10)
+                OR (te.np_communication_id = 'NPWR12464_00' AND te.group_id = 'default' AND te.order_id = 11)
                 ) AND p.status = 0 GROUP BY online_id) x USING (np_communication_id)
             ORDER BY player_name");
         $query->execute();
