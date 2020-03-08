add21c952e4cc6ab23f8474245629f713cdbcf94
Ragowit <markus.persson@grh.se>
2020-03-08 22:35:19 +0100

    Admin - Possible Cheaters One Way Trip

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -193,6 +193,8 @@ require_once("../init.php");
                 OR (te.np_communication_id = 'NPWR18341_00' AND te.group_id = 'default' AND te.order_id = 35)
                 OR (te.np_communication_id = 'NPWR05357_00' AND te.group_id = 'default' AND te.order_id = 0)
                 OR (te.np_communication_id = 'NPWR05357_00' AND te.group_id = 'default' AND te.order_id = 10)
+                OR (te.np_communication_id = 'NPWR11687_00' AND te.group_id = 'default' AND te.order_id = 0)
+                OR (te.np_communication_id = 'NPWR11687_00' AND te.group_id = 'default' AND te.order_id = 6)
                 ) AND p.status = 0 GROUP BY online_id) x USING (np_communication_id)
             ORDER BY player_name");
         $query->execute();
