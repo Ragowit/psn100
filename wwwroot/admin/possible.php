34875e823ed3792f8eb22a0fa43eb5d33b3c7843
Ragowit <markus.persson@grh.se>
2020-03-08 21:18:07 +0100

    Admin - Possible Cheaters NBA 2K17 (PS4)

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -187,6 +187,8 @@ require_once("../init.php");
                 OR (te.np_communication_id = 'NPWR17151_00' AND te.group_id = 'default' AND te.order_id = 0)
                 OR (te.np_communication_id = 'NPWR17151_00' AND te.group_id = 'default' AND te.order_id = 21)
                 OR (te.np_communication_id = 'NPWR17151_00' AND te.group_id = 'default' AND te.order_id = 22)
+                OR (te.np_communication_id = 'NPWR11010_00' AND te.group_id = 'default' AND te.order_id = 0)
+                OR (te.np_communication_id = 'NPWR11010_00' AND te.group_id = 'default' AND te.order_id = 22)
                 ) AND p.status = 0 GROUP BY online_id) x USING (np_communication_id)
             ORDER BY player_name");
         $query->execute();
