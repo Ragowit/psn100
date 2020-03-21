9cdc3b7037db3b6a59c0de3eded509374a632b60
Ragowit <markus.persson@grh.se>
2020-03-21 23:03:04 +0100

    Admin - Possible Cheaters Super Mutant Alien Assault (PS4)

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -261,6 +261,7 @@ require_once("../init.php");
                 OR (te.np_communication_id = 'NPWR12063_00' AND te.group_id = '001' AND te.order_id = 31)
                 OR (te.np_communication_id = 'NPWR12063_00' AND te.group_id = '001' AND te.order_id = 32)
                 OR (te.np_communication_id = 'NPWR12063_00' AND te.group_id = '001' AND te.order_id = 33)
+                OR (te.np_communication_id = 'NPWR11013_00' AND te.group_id = 'default' AND te.order_id = 16)
                 ) AND p.status = 0 GROUP BY online_id) x USING (np_communication_id)
             ORDER BY player_name");
         $query->execute();
