bf0bfd6fcdb6a3278b8ff3062cfd8f4ea9b01416
Ragowit <markus.persson@grh.se>
2020-04-26 13:14:57 +0200

    Admin - Cron Attempts to fix deadlocks. And SQL Beautifier.

--- wwwroot/cron/daily.php
+++ wwwroot/cron/daily.php
@@ -5,9 +5,18 @@ set_time_limit(0);
 require_once("/home/psn100/public_html/init.php");
 
 // Update ranks
-$query = $database->prepare("SELECT account_id, points, country, level FROM player
-    WHERE status = 0
-    ORDER BY points DESC, platinum DESC, gold DESC, silver DESC, bronze DESC, level DESC");
+$query = $database->prepare("SELECT account_id,
+           points,
+           country,
+           level
+    FROM   player
+    WHERE  status = 0
+    ORDER  BY points DESC,
+              platinum DESC,
+              gold DESC,
+              silver DESC,
+              bronze DESC,
+              level DESC ");
 $query->execute();
 $rank = 1;
 $rowCount = 0;
@@ -37,7 +46,10 @@ while ($player = $query->fetch()) {
 
     $previousPlayerPoints = $playerPoints;
 
-    $queryRank = $database->prepare("UPDATE player SET rank = :rank, rank_country = :rank_country WHERE account_id = :account_id");
+    $queryRank = $database->prepare("UPDATE player
+        SET    rank = :rank,
+               rank_country = :rank_country
+        WHERE  account_id = :account_id ");
     $queryRank->bindParam(":rank", $rank, PDO::PARAM_INT);
     $queryRank->bindParam(":rank_country", $countryRanks[$player["country"]][1], PDO::PARAM_INT);
     $queryRank->bindParam(":account_id", $player["account_id"], PDO::PARAM_INT);
@@ -45,123 +57,174 @@ while ($player = $query->fetch()) {
 }
 
 // Recalculate trophy rarity.
-$query = $database->prepare("UPDATE trophy t SET t.rarity_percent = CASE
-    WHEN (SELECT owners FROM trophy_title tt WHERE tt.np_communication_id = t.np_communication_id) = 0 THEN 0
-    WHEN (SELECT owners FROM trophy_title tt WHERE tt.np_communication_id = t.np_communication_id) <= (
-        SELECT COUNT(*) FROM trophy_earned te
-        JOIN player p USING (account_id)
-        WHERE te.np_communication_id = t.np_communication_id AND te.group_id = t.group_id AND te.order_id = t.order_id AND p.status = 0 AND p.rank <= 100000
-        ) THEN 100
-    ELSE
-        ((SELECT COUNT(*) FROM trophy_earned te
-        JOIN player p USING (account_id)
-        WHERE te.np_communication_id = t.np_communication_id AND te.group_id = t.group_id AND te.order_id = t.order_id AND p.status = 0 AND p.rank <= 100000
-        ) / (
-            SELECT owners FROM trophy_title tt WHERE tt.np_communication_id = t.np_communication_id
-        ) * 100)
-    END");
-$query->execute();
+do {
+    try {
+        $query = $database->prepare("UPDATE trophy t
+            SET    t.rarity_percent = CASE
+                                        WHEN (SELECT owners
+                                              FROM   trophy_title tt
+                                              WHERE  tt.np_communication_id =
+                                             t.np_communication_id) = 0 THEN 0
+                                        WHEN (SELECT owners
+                                              FROM   trophy_title tt
+                                              WHERE  tt.np_communication_id =
+                                             t.np_communication_id) <=
+                                                    (SELECT Count(*)
+                                                     FROM
+                                                    trophy_earned te
+                                                    JOIN player p USING (account_id)
+                                                    WHERE
+                                                    te.np_communication_id =
+                                                    t.np_communication_id
+                                                    AND te.group_id = t.group_id
+                                                    AND te.order_id = t.order_id
+                                                    AND p.status = 0
+                                                    AND p.rank <= 100000) THEN 100
+                                        ELSE ( (SELECT Count(*)
+                                                FROM   trophy_earned te
+                                                       JOIN player p USING (account_id)
+                                                WHERE  te.np_communication_id =
+                                                       t.np_communication_id
+                                                       AND te.group_id = t.group_id
+                                                       AND te.order_id = t.order_id
+                                                       AND p.status = 0
+                                                       AND p.rank <= 100000) / (SELECT
+                                               owners
+                                                                                FROM
+                                               trophy_title tt
+                                                                                WHERE
+                                                        tt.np_communication_id =
+                                                        t.np_communication_id) * 100 )
+                                      end ");
+        $query->execute();
+
+        $deadlock = false;
+    } catch (Exception $e) {
+        sleep(3);
+        $deadlock = true;
+    }
+} while ($deadlock);
 
 // Recalculate trophy rarity point
-$query = $database->prepare("UPDATE trophy t
-    JOIN trophy_title tt USING (np_communication_id)
-    SET t.rarity_point = IF(t.status = 1 OR tt.status != 0, 0, FLOOR(1 / (GREATEST(t.rarity_percent, 0.01) / 100) - 1))");
-$query->execute();
+do {
+    try {
+        $query = $database->prepare("UPDATE trophy t
+                   JOIN trophy_title tt USING (np_communication_id)
+            SET    t.rarity_point = IF(t.status = 1
+                                        OR tt.status != 0, 0, Floor(1 / (
+                                                              Greatest(t.rarity_percent,
+                                                              0.01) /
+                                                              100 )
+                                                                    - 1)) ");
+        $query->execute();
+
+        $deadlock = false;
+    } catch (Exception $e) {
+        sleep(3);
+        $deadlock = true;
+    }
+} while ($deadlock);
 
 // Recalculate rarity points and rarity levels for each players.
 $query = $database->prepare("SELECT account_id FROM player WHERE status = 0");
 $query->execute();
 while ($row = $query->fetch()) {
     // Rarity points for each game
-    $queryUpdate = $database->prepare("UPDATE
-            trophy_title_player ttp
-        INNER JOIN(
-            SELECT
-                account_id,
-                np_communication_id,
-                SUM(rarity_point) AS points
-            FROM
-                trophy_earned
-            JOIN trophy USING(
-                    np_communication_id,
-                    group_id,
-                    order_id
-                )
-            WHERE
-                account_id = :account_id
-            GROUP BY
-                np_communication_id
-        ) x USING(
-            account_id,
-            np_communication_id
-        )
-        SET
-            ttp.rarity_points = x.points");
-    $queryUpdate->bindParam(":account_id", $row["account_id"], PDO::PARAM_INT);
-    $queryUpdate->execute();
+    do {
+        try {
+            $queryUpdate = $database->prepare("UPDATE trophy_title_player ttp
+                       INNER JOIN(SELECT account_id,
+                                         np_communication_id,
+                                         Sum(rarity_point) AS points
+                                  FROM   trophy_earned
+                                         JOIN trophy USING( np_communication_id, group_id,
+                                                            order_id )
+                                  WHERE  account_id = :account_id
+                                  GROUP  BY np_communication_id) x USING(
+                       account_id, np_communication_id )
+                SET    ttp.rarity_points = x.points ");
+            $queryUpdate->bindParam(":account_id", $row["account_id"], PDO::PARAM_INT);
+            $queryUpdate->execute();
+
+            $deadlock = false;
+        } catch (Exception $e) {
+            sleep(3);
+            $deadlock = true;
+        }
+    } while ($deadlock);
 
     // Total rarity points
-    $queryUpdate = $database->prepare("UPDATE
-            player p
-        INNER JOIN(
-            SELECT account_id,
-                SUM(rarity_points) AS rarity_points
-            FROM
-                trophy_title_player
-            WHERE
-                account_id = :account_id
-            GROUP BY
-                account_id
-        ) ttp USING(account_id)
-        SET
-            p.rarity_points = ttp.rarity_points");
-    $queryUpdate->bindParam(":account_id", $row["account_id"], PDO::PARAM_INT);
-    $queryUpdate->execute();
+    do {
+        try {
+            $queryUpdate = $database->prepare("UPDATE player p
+                       INNER JOIN(SELECT account_id,
+                                         Sum(rarity_points) AS rarity_points
+                                  FROM   trophy_title_player
+                                  WHERE  account_id = :account_id
+                                  GROUP  BY account_id) ttp USING(account_id)
+                SET    p.rarity_points = ttp.rarity_points ");
+            $queryUpdate->bindParam(":account_id", $row["account_id"], PDO::PARAM_INT);
+            $queryUpdate->execute();
+
+            $deadlock = false;
+        } catch (Exception $e) {
+            sleep(3);
+            $deadlock = true;
+        }
+    } while ($deadlock);
 
     // Rarity levels
-    $queryUpdate = $database->prepare("UPDATE
-                player p
-            INNER JOIN (
-            SELECT
-                te.account_id,
-                SUM(t.rarity_percent > 50) common,
-                SUM(
-                    t.rarity_percent <= 50 AND t.rarity_percent > 20
-                ) uncommon,
-                SUM(
-                    t.rarity_percent <= 20 AND t.rarity_percent > 5
-                ) rare,
-                SUM(
-                    t.rarity_percent <= 5 AND t.rarity_percent > 1
-                ) epic,
-                SUM(t.rarity_percent <= 1) legendary
-            FROM
-                trophy_earned te
-            JOIN trophy t USING(
-                    np_communication_id,
-                    group_id,
-                    order_id
-                )
-            JOIN trophy_title tt USING(np_communication_id)
-            WHERE
-                te.account_id = :account_id AND t.status = 0 AND tt.status = 0
-            GROUP BY
-                te.account_id
-        ) x USING(account_id)
-        SET
-            p.common = x.common,
-            p.uncommon = x.uncommon,
-            p.rare = x.rare,
-            p.epic = x.epic,
-            p.legendary = x.legendary");
-    $queryUpdate->bindParam(":account_id", $row["account_id"], PDO::PARAM_INT);
-    $queryUpdate->execute();
+    do {
+        try {
+            $queryUpdate = $database->prepare("UPDATE player p
+                       INNER JOIN (SELECT te.account_id,
+                                          Sum(t.rarity_percent > 50)     common,
+                                          Sum(t.rarity_percent <= 50
+                                              AND t.rarity_percent > 20) uncommon,
+                                          Sum(t.rarity_percent <= 20
+                                              AND t.rarity_percent > 5)  rare,
+                                          Sum(t.rarity_percent <= 5
+                                              AND t.rarity_percent > 1)  epic,
+                                          Sum(t.rarity_percent <= 1)     legendary
+                                   FROM   trophy_earned te
+                                          JOIN trophy t USING( np_communication_id, group_id,
+                                                               order_id
+                                          )
+                                          JOIN trophy_title tt USING(np_communication_id)
+                                   WHERE  te.account_id = :account_id
+                                          AND t.status = 0
+                                          AND tt.status = 0
+                                   GROUP  BY te.account_id) x USING(account_id)
+                SET    p.common = x.common,
+                       p.uncommon = x.uncommon,
+                       p.rare = x.rare,
+                       p.epic = x.epic,
+                       p.legendary = x.legendary ");
+            $queryUpdate->bindParam(":account_id", $row["account_id"], PDO::PARAM_INT);
+            $queryUpdate->execute();
+
+            $deadlock = false;
+        } catch (Exception $e) {
+            sleep(3);
+            $deadlock = true;
+        }
+    } while ($deadlock);
 }
 
 // Update rarity ranks
-$query = $database->prepare("SELECT account_id, rarity_points, country, level FROM player
-    WHERE status = 0
-    ORDER BY rarity_points DESC, legendary DESC, epic DESC, rare DESC, uncommon DESC, common DESC, level DESC");
+$query = $database->prepare("SELECT account_id,
+           rarity_points,
+           country,
+           level
+    FROM   player
+    WHERE  status = 0
+    ORDER  BY rarity_points DESC,
+              legendary DESC,
+              epic DESC,
+              rare DESC,
+              uncommon DESC,
+              common DESC,
+              level DESC ");
 $query->execute();
 $rank = 1;
 $rowCount = 0;
@@ -191,7 +254,10 @@ while ($player = $query->fetch()) {
 
     $previousPlayerRarityPoints = $playerRarityPoints;
 
-    $queryRank = $database->prepare("UPDATE player SET rarity_rank = :rarity_rank, rarity_rank_country = :rarity_rank_country WHERE account_id = :account_id");
+    $queryRank = $database->prepare("UPDATE player
+        SET    rarity_rank = :rarity_rank,
+               rarity_rank_country = :rarity_rank_country
+        WHERE  account_id = :account_id ");
     $queryRank->bindParam(":rarity_rank", $rank, PDO::PARAM_INT);
     $queryRank->bindParam(":rarity_rank_country", $countryRanks[$player["country"]][1], PDO::PARAM_INT);
     $queryRank->bindParam(":account_id", $player["account_id"], PDO::PARAM_INT);
--- wwwroot/cron/hourly.php
+++ wwwroot/cron/hourly.php
@@ -5,31 +5,64 @@ set_time_limit(0);
 require_once("/home/psn100/public_html/init.php");
 
 // Recalculate owners for each game, but only for those ranked 100k or higher
-$query = $database->prepare("UPDATE trophy_title tt SET tt.owners = (
-    SELECT COUNT(*) FROM trophy_title_player ttp
-    JOIN player p USING (account_id)
-    WHERE ttp.np_communication_id = tt.np_communication_id AND p.status = 0 AND p.rank <= 100000)");
-$query->execute();
+do {
+    try {
+        $query = $database->prepare("UPDATE trophy_title tt
+            SET    tt.owners = (SELECT Count(*)
+                                FROM   trophy_title_player ttp
+                                       JOIN player p USING(account_id)
+                                WHERE  ttp.np_communication_id = tt.np_communication_id
+                                       AND p.status = 0
+                                       AND p.rank <= 100000) ");
+        $query->execute();
+
+        $deadlock = false;
+    } catch (Exception $e) {
+        sleep(3);
+        $deadlock = true;
+    }
+} while ($deadlock);
 
 // Update game difficulty
-$query = $database->prepare("UPDATE trophy_title tt SET tt.difficulty = CASE WHEN tt.owners = 0 THEN 0 ELSE ((
-    SELECT COUNT(*) FROM trophy_title_player ttp
-    JOIN player p USING (account_id)
-    WHERE p.status = 0 AND p.rank <= 100000 AND ttp.progress = 100 AND ttp.np_communication_id = tt.np_communication_id
-    ) / tt.owners
-) * 100 END");
-$query->execute();
+do {
+    try {
+        $query = $database->prepare("UPDATE trophy_title tt
+            SET    tt.difficulty = CASE
+                                     WHEN tt.owners = 0 THEN 0
+                                     ELSE( (SELECT Count(*)
+                                            FROM   trophy_title_player ttp
+                                                   JOIN player p USING(account_id)
+                                            WHERE  p.status = 0
+                                                   AND p.rank <= 100000
+                                                   AND ttp.progress = 100
+                                                   AND ttp.np_communication_id =
+                                                       tt.np_communication_id) /
+                                           tt.owners ) * 100
+                                   end ");
+        $query->execute();
+
+        $deadlock = false;
+    } catch (Exception $e) {
+        sleep(3);
+        $deadlock = true;
+    }
+} while ($deadlock);
 
 // Recalculate recent players
-$select = $database->prepare("SELECT np_communication_id, COUNT(*) AS count
-    FROM trophy_title_player ttp
-    JOIN player p USING (account_id)
-    WHERE p.status = 0 AND p.rank <= 1000000 AND ttp.last_updated_date >= DATE(NOW()) - INTERVAL 7 DAY
-    GROUP BY np_communication_id");
+$select = $database->prepare("SELECT np_communication_id,
+           Count(*) AS recent_players
+    FROM   trophy_title_player ttp
+           JOIN player p USING(account_id)
+    WHERE  p.status = 0
+           AND p.rank <= 1000000
+           AND ttp.last_updated_date >= Date(Now()) - INTERVAL 7 day
+    GROUP  BY np_communication_id ");
 $select->execute();
 while ($row = $select->fetch()) {
-    $update = $database->prepare("UPDATE trophy_title SET recent_players = :recent_players WHERE np_communication_id = :np_communication_id");
-    $update->bindParam(":recent_players", $row["count"], PDO::PARAM_INT);
+    $update = $database->prepare("UPDATE trophy_title 
+        SET    recent_players = :recent_players
+        WHERE  np_communication_id = :np_communication_id ");
+    $update->bindParam(":recent_players", $row["recent_players"], PDO::PARAM_INT);
     $update->bindParam(":np_communication_id", $row["np_communication_id"], PDO::PARAM_STR);
     $update->execute();
 }
