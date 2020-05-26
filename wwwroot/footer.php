745e649420a5c0d8cf898f94b5cf197686487980
Ragowit <markus.persson@grh.se>
2020-05-26 23:54:05 +0200

    2.14.9

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -8,7 +8,7 @@
                     <a href="https://github.com/Ragowit/psn100/issues">Issues</a>
                 </div>
                 <div class="col-md-4 text-right">
-                    <a href="https://github.com/Ragowit/psn100/releases">v2.14.8</a>
+                    <a href="https://github.com/Ragowit/psn100/releases">v2.14.9</a>
                 </div>
             </div>
         </footer>
