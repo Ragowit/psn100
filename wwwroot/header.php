1ecec45dedf8ed0f19fe974c5da289d780b1f000
Ragowit <markus.persson@grh.se>
2020-05-28 22:44:41 +0200

    Favicon from https://favicon.io/

--- wwwroot/header.php
+++ wwwroot/header.php
@@ -18,6 +18,10 @@
         <meta name="description" content="Check your leaderboard position against other PlayStation trophy hunters!">
         <meta name="author" content="Markus 'Ragowit' Persson, and other contributors via GitHub project">
 
+        <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
