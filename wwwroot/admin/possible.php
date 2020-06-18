26538620ac6139fc500e7cbba45043c30b37226b
Ragowit <markus.persson@grh.se>
2020-06-18 17:14:58 +0200

    Admin - Possible Cheaters Added checks for Hive: Altenum Wars (NA)

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -340,6 +340,9 @@ require_once("../init.php");
         // Titan Quest
         $sql .= " OR (te.np_communication_id = 'NPWR13165_00' AND te.group_id = '001' AND te.order_id = 31)
             OR (te.np_communication_id = 'NPWR13165_00' AND te.group_id = '002' AND te.order_id = 39)";
+        // Hive: Altenum Wars (NA)
+        $sql .= " OR (te.np_communication_id = 'NPWR15369_00' AND te.group_id = 'default' AND te.order_id = 0)
+            OR (te.np_communication_id = 'NPWR15369_00' AND te.group_id = 'default' AND te.order_id = 16)";
         $sql .= ") AND p.status = 0 GROUP BY online_id) x USING (np_communication_id)
         ORDER BY player_name";
 
