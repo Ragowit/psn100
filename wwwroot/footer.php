cc51ec650b1ae9defbba3273e78494fd9c8f7b87
Ragowit <markus.persson@grh.se>
2020-08-22 00:02:41 +0200

    3.13.7

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -8,7 +8,7 @@
                     <a href="https://github.com/Ragowit/psn100/issues">Issues</a>
                 </div>
                 <div class="col-md-4 text-right">
-                    <a href="https://github.com/Ragowit/psn100/releases">v3.13.6</a>
+                    <a href="https://github.com/Ragowit/psn100/releases">v3.13.7</a>
                 </div>
             </div>
         </footer>
