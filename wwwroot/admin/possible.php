67941fc166b39ae3d1ddf26df9ec67ff15707e1a
Markus Persson <github@grh.se>
2020-11-26 15:12:45 +0100

    Admin - Possible Cheaters
    Removed Hotshot Racing, as it seems patched.

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -343,10 +343,6 @@ require_once("../init.php");
         $sql .= " OR (te.np_communication_id = 'NPWR19903_00' AND te.group_id = 'default' AND te.order_id = 0 AND te.earned_date < '2020-09-01')
             OR (te.np_communication_id = 'NPWR19903_00' AND te.group_id = 'default' AND te.order_id = 21 AND te.earned_date < '2020-09-01')
             OR (te.np_communication_id = 'NPWR19903_00' AND te.group_id = 'default' AND te.order_id = 27 AND te.earned_date < '2020-09-01')";
-        // Hotshot Racing
-        $sql .= " OR (te.np_communication_id = 'NPWR15213_00' AND te.group_id = 'default' AND te.order_id = 0)
-            OR (te.np_communication_id = 'NPWR15213_00' AND te.group_id = 'default' AND te.order_id = 43)
-            OR (te.np_communication_id = 'NPWR15213_00' AND te.group_id = 'default' AND te.order_id = 44)";
         // Marvel's Avengers
         $sql .= " OR (te.np_communication_id = 'NPWR16769_00' AND te.group_id = 'default' AND te.order_id = 0 AND te.earned_date < '2020-09-19')
             OR (te.np_communication_id = 'NPWR16769_00' AND te.group_id = 'default' AND te.order_id = 7 AND te.earned_date < '2020-09-19')";
