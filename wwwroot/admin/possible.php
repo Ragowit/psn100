eb951adb6055272161a360f55fdb16f1d550e864
Markus Persson <github@grh.se>
2020-11-30 16:45:19 +0100

    Admin - Possible Cheaters
    One Dog Story

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -350,10 +350,6 @@ require_once("../init.php");
         $sql .= " OR (te.np_communication_id = 'MERGE_011589' AND te.group_id = '001' AND te.order_id = 34)
             OR (te.np_communication_id = 'MERGE_011589' AND te.group_id = '001' AND te.order_id = 35)
             OR (te.np_communication_id = 'MERGE_011589' AND te.group_id = '001' AND te.order_id = 36)";
-        // One Dog Story
-        $sql .= " OR (te.np_communication_id = 'MERGE_011625' AND te.group_id = 'default' AND te.order_id = 0)
-            OR (te.np_communication_id = 'MERGE_011625' AND te.group_id = 'default' AND te.order_id = 17)
-            OR (te.np_communication_id = 'MERGE_011625' AND te.group_id = 'default' AND te.order_id = 24)";
         $sql .= ") AND p.status != 1 GROUP BY online_id) x USING (np_communication_id)
         ORDER BY player_name";
 
