d4bf3ce969e194ad50a8938d88a053b125257557
Markus Persson <github@grh.se>
2020-12-16 16:58:54 +0100

    5.0.3

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -8,7 +8,7 @@
                     <a href="https://github.com/Ragowit/psn100/issues">Issues</a>
                 </div>
                 <div class="col-md-4 text-right">
-                    <a href="https://github.com/Ragowit/psn100/releases">v5.0.2</a>
+                    <a href="https://github.com/Ragowit/psn100/releases">v5.0.3</a>
                 </div>
             </div>
         </footer>
