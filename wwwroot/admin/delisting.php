fea2e36af3a2021fdb0d36fe2c7a2deeec19c962
Ragowit <markus.persson@grh.se>
2021-01-25 18:41:50 +0100

    Admin - New game status. Obsolete.

--- wwwroot/admin/delisting.php
+++ wwwroot/admin/delisting.php
@@ -13,20 +13,38 @@ if (isset($_POST["game"]) && ctype_digit(strval($_POST["game"]))) {
     $database->commit();
 
     // Add all affected players to the queue to recalculate trophy count, level and level progress
-    $players = $database->prepare("SELECT online_id FROM player p WHERE EXISTS (SELECT 1 FROM trophy_title_player ttp WHERE ttp.np_communication_id = (SELECT np_communication_id FROM trophy_title WHERE id = :game_id) AND ttp.account_id = p.account_id)");
-    $players->bindParam(":game_id", $gameId, PDO::PARAM_INT);
-    $players->execute();
-    while ($player = $players->fetch()) {
-        $query = $database->prepare("INSERT IGNORE INTO player_queue (online_id, request_time) VALUES (:online_id, :request_time)");
-        $query->bindParam(":online_id", $player["online_id"], PDO::PARAM_STR);
-        $query->bindValue(":request_time", "2030-12-24 00:00:00", PDO::PARAM_STR);
-        $query->execute();
-    }
+    $query = $database->prepare("INSERT IGNORE
+        INTO player_queue(online_id, request_time)
+        SELECT
+            online_id,
+            '2030-12-24 00:00:00'
+        FROM
+            player p
+        WHERE EXISTS
+            (
+            SELECT
+                1
+            FROM
+                trophy_title_player ttp
+            WHERE
+                ttp.np_communication_id =(
+                SELECT
+                    np_communication_id
+                FROM
+                    trophy_title
+                WHERE
+                    id = :game_id
+            ) AND ttp.account_id = p.account_id
+        )");
+    $query->bindParam(":game_id", $gameId, PDO::PARAM_INT);
+    $query->execute();
 
     if ($status == 1) {
         $statusText = "delisted";
+    } elseif ($status == 3) {
+        $statusText = "obsolete";
     } else {
-        $statusText = "relisted";
+        $statusText = "normal";
     }
 
     $success = "<p>Game ". $gameId ." is now set as ". $statusText .". All affected players will be updated soon, and ranks updated the next whole hour.</p>";
@@ -39,7 +57,7 @@ if (isset($_POST["game"]) && ctype_digit(strval($_POST["game"]))) {
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title>Admin ~ Delisted Game</title>
+        <title>Admin ~ Game Status</title>
     </head>
     <body>
         <a href="/admin/">Back</a><br><br>
@@ -48,8 +66,9 @@ if (isset($_POST["game"]) && ctype_digit(strval($_POST["game"]))) {
             <input type="number" name="game"><br>
             Status:<br>
             <select name="status">
+                <option value="0">Normal</option>
                 <option value="1">Delisted</option>
-                <option value="0">Relisted</option>
+                <option value="3">Obsolete</option>
             </select><br><br>
             <input type="submit" value="Submit">
         </form>
