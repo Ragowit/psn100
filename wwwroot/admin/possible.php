addf4e12f3e11adda0ef5be25a945c3e807e5d93
Markus Persson <github@grh.se>
2021-02-03 10:07:26 +0100

    Admin - Possible Cheater
    Removed Wasteland 3, it have been patched and fixed.

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -330,10 +330,6 @@ require_once("../init.php");
         // Marvel's Avengers
         $sql .= " OR (te.np_communication_id = 'NPWR16769_00' AND te.group_id = 'default' AND te.order_id = 0 AND te.earned_date < '2020-09-19')
             OR (te.np_communication_id = 'NPWR16769_00' AND te.group_id = 'default' AND te.order_id = 7 AND te.earned_date < '2020-09-19')";
-        // Wasteland 3
-        $sql .= " OR (te.np_communication_id = 'NPWR19007_00' AND te.group_id = 'default' AND te.order_id = 0)
-            OR (te.np_communication_id = 'NPWR19007_00' AND te.group_id = 'default' AND te.order_id = 30)
-            OR (te.np_communication_id = 'NPWR19007_00' AND te.group_id = 'default' AND te.order_id = 60)";
         // Tokyo Tattoo Girls
         $sql .= " OR (te.np_communication_id = 'NPWR14063_00' AND te.group_id = 'default' AND te.order_id = 0)
             OR (te.np_communication_id = 'NPWR14063_00' AND te.group_id = 'default' AND te.order_id = 35)";
