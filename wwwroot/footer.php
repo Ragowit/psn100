f85cb6022b9457aeb606dcdcfe5139dd10a85bb8
Markus Persson <github@grh.se>
2021-03-02 16:56:46 +0100

    6.11.3

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -8,7 +8,7 @@
                     <a href="https://github.com/Ragowit/psn100/issues">Issues</a>
                 </div>
                 <div class="col-md-4 text-right">
-                    <a href="https://github.com/Ragowit/psn100/releases">v6.11.2</a>
+                    <a href="https://github.com/Ragowit/psn100/releases">v6.11.3</a>
                 </div>
             </div>
         </footer>
