0378151938836c80660c7d9b4700ebaf9cbe2b2d
Ragowit <markus.persson@grh.se>
2021-10-06 21:12:04 +0200

    6.28.0

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -8,7 +8,7 @@
                     <a href="https://github.com/Ragowit/psn100/issues">Issues</a>
                 </div>
                 <div class="col-md-4 text-right">
-                    <a href="https://github.com/Ragowit/psn100/releases">v6.27.3</a>
+                    <a href="https://github.com/Ragowit/psn100/releases">v6.28.0</a>
                 </div>
             </div>
         </footer>
