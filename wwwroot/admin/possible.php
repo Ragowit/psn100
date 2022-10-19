12f841e3d42d83e9216ffb11a182c9977f38e852
Ragowit <github@grh.se>
2022-10-19 21:24:00 +0200

    Possible Cheaters Added checks for Lost Planet 2

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -458,5 +458,22 @@ require_once("../init.php");
             echo "<a href=\"/game/6489-kyasarinfurubodi/". $possibleCheater["online_id"] ."\">". $possibleCheater["online_id"] ." (". $possibleCheater["account_id"] .")</a><br>";
         }
         ?>
+        <br>
+        Lost Planet 2<br>
+        <?php
+        $query = $database->prepare("SELECT account_id, online_id, TIMESTAMPDIFF(SECOND, first_trophy, second_trophy) time_difference
+            FROM player p
+            JOIN (SELECT earned_date AS first_trophy, account_id FROM trophy_earned WHERE np_communication_id = 'NPWR00928_00' AND group_id = 'default' AND order_id = 47) trophy_start USING (account_id)
+            JOIN (SELECT earned_date AS second_trophy, account_id FROM trophy_earned WHERE np_communication_id = 'NPWR00928_00' AND group_id = 'default' AND order_id = 50) trophy_end USING (account_id)
+            WHERE p.status != 1
+            HAVING time_difference <= 0
+            ORDER BY online_id");
+        $query->execute();
+        $possibleCheaters = $query->fetchAll();
+
+        foreach ($possibleCheaters as $possibleCheater) {
+            echo "<a href=\"/game/4237-lost-planet-2/". $possibleCheater["online_id"] ."\">". $possibleCheater["online_id"] ." (". $possibleCheater["account_id"] .")</a><br>";
+        }
+        ?>
     </body>
 </html>
