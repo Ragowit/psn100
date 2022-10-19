50eac1857d91d6e47c2a6e4906a52850a0545451
Ragowit <github@grh.se>
2022-10-19 23:20:36 +0200

    Possible Cheaters Added checks for Terminator Salvation

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -475,5 +475,22 @@ require_once("../init.php");
             echo "<a href=\"/game/4237-lost-planet-2/". $possibleCheater["online_id"] ."\">". $possibleCheater["online_id"] ." (". $possibleCheater["account_id"] .")</a><br>";
         }
         ?>
+        <br>
+        Terminator Salvation<br>
+        <?php
+        $query = $database->prepare("SELECT account_id, online_id, TIMESTAMPDIFF(SECOND, first_trophy, second_trophy) time_difference
+            FROM player p
+            JOIN (SELECT earned_date AS first_trophy, account_id FROM trophy_earned WHERE np_communication_id = 'NPWR00623_00' AND group_id = 'default' AND order_id = 8) trophy_start USING (account_id)
+            JOIN (SELECT earned_date AS second_trophy, account_id FROM trophy_earned WHERE np_communication_id = 'NPWR00623_00' AND group_id = 'default' AND order_id = 9) trophy_end USING (account_id)
+            WHERE p.status != 1
+            HAVING time_difference <= 0
+            ORDER BY online_id");
+        $query->execute();
+        $possibleCheaters = $query->fetchAll();
+
+        foreach ($possibleCheaters as $possibleCheater) {
+            echo "<a href=\"/game/294-terminator-salvation/". $possibleCheater["online_id"] ."\">". $possibleCheater["online_id"] ." (". $possibleCheater["account_id"] .")</a><br>";
+        }
+        ?>
     </body>
 </html>
