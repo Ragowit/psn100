a357c84ac024767a59b025cae7a7144ded767bf0
Ragowit <github@grh.se>
2023-01-19 21:00:11 +0100

    Scanner No longer accepts players tagged as cheaters.

--- wwwroot/add_to_queue.php
+++ wwwroot/add_to_queue.php
@@ -14,8 +14,24 @@ $query->bindParam(":ip_address", $_SERVER["REMOTE_ADDR"], PDO::PARAM_STR);
 $query->execute();
 $count = $query->fetchColumn();
 
+// Check cheater status
+$query = $database->prepare("SELECT
+        account_id
+    FROM
+        player
+    WHERE
+        online_id = :online_id AND status = 1
+    ");
+$query->bindParam(":online_id", $player, PDO::PARAM_STR);
+$query->execute();
+$accountId = $query->fetchColumn();
+
 if (!isset($player) || $player === "") {
     echo "PSN name can't be empty.";
+} elseif ($accountId !== false) {
+    ?>
+    Player '<a href="/player/<?= $player; ?>"><?= $player; ?></a>' is tagged as a cheater and won't be scanned. <a href="https://github.com/Ragowit/psn100/issues?q=label%3Acheater+<?= $player; ?>+OR+<?= $accountId; ?>">Dispute</a>?
+    <?php
 } elseif ($count >= 50) {
     echo "You have already entered 50 players into the queue. Please wait a while.";
 } elseif (preg_match("/^[\w\-]{3,16}$/", $player)) {
