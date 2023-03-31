63d431bb1e25ea36567315c7580c405bd6bd540d
Ragowit <github@grh.se>
2023-03-31 21:24:47 +0200

    6.70.822

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -8,7 +8,7 @@
                     <a href="https://github.com/Ragowit/psn100/issues">Issues</a>
                 </div>
                 <div class="col-md-4 text-right">
-                    <a href="https://github.com/Ragowit/psn100/releases">v6.70.821</a>
+                    <a href="https://github.com/Ragowit/psn100/releases">v6.70.822</a>
                 </div>
             </div>
         </footer>
