4f9cd675d841cf5db625b76a22be5e651e9bf6fb
Ragowit <github@grh.se>
2023-09-28 00:03:04 +0200

    6.77

--- wwwroot/footer.php
+++ wwwroot/footer.php
@@ -9,7 +9,7 @@
                 </div>
                 <div class="col-md-4 text-right">
                     <a href="/changelog">Changelog</a><br>
-                    <a href="https://github.com/Ragowit/psn100/releases">v6.76.906</a>
+                    <a href="https://github.com/Ragowit/psn100/releases">v6.77</a>
                 </div>
             </div>
         </footer>
