dc36839311becd3d2def4a28b61ebb516ae20d39
Ragowit <github@grh.se>
2023-10-04 20:45:01 +0200

    Admin - Possible Cheaters New check against Angry Birds Trilogy [PS3]

--- wwwroot/admin/possible.php
+++ wwwroot/admin/possible.php
@@ -556,6 +556,23 @@ require_once("../init.php");
         ?>
 
         <br>
+        Angry Birds Trilogy [PS3] (Block Breaker <-> Block Annihilator)<br>
+        <?php
+        $query = $database->prepare("SELECT account_id, online_id, ABS(TIMESTAMPDIFF(SECOND, first_trophy, second_trophy)) time_difference
+            FROM player p
+            JOIN (SELECT earned_date AS first_trophy, account_id FROM trophy_earned WHERE np_communication_id = 'NPWR03771_00' AND order_id = 30) abt_start USING (account_id)
+            JOIN (SELECT earned_date AS second_trophy, account_id FROM trophy_earned WHERE np_communication_id = 'NPWR03771_00' AND order_id = 31) abt_end USING (account_id)
+            WHERE p.status != 1
+            HAVING time_difference <= 60
+            ORDER BY online_id");
+        $query->execute();
+        $possibleCheaters = $query->fetchAll();
+        foreach ($possibleCheaters as $possibleCheater) {
+            echo "<a href=\"/game/3810-angry-birds-trilogy/". $possibleCheater["online_id"] ."?order=date\">". $possibleCheater["online_id"] ." (". $possibleCheater["account_id"] .")</a><br>";
+        }
+        ?>
+
+        <br>
         Terminator Salvation<br>
         <?php
         $query = $database->prepare("SELECT
