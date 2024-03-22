5c173db180d302b90e7b1d0ab46b4eb6c878fb30
Ragowit <github@grh.se>
2024-03-22 09:17:15 +0100

    Scanning Improve message if player being scanned is a cheater.

--- wwwroot/check_queue_position.php
+++ wwwroot/check_queue_position.php
@@ -4,13 +4,7 @@ require_once("init.php");
 $player = trim($_REQUEST["q"]);
 
 // Check if scanning
-$query = $database->prepare("SELECT
-        scanning
-    FROM
-        setting
-    WHERE
-        scanning = :online_id
-");
+$query = $database->prepare("SELECT scanning FROM setting WHERE scanning = :online_id");
 $query->bindParam(":online_id", $player, PDO::PARAM_STR);
 $query->execute();
 $scanning = $query->fetchColumn();
@@ -38,9 +32,19 @@ $query->bindParam(":online_id", $player, PDO::PARAM_STR);
 $query->execute();
 $position = $query->fetchColumn();
 
+// Check status
+$query = $database->prepare("SELECT `status` FROM player WHERE online_id = :online_id");
+$query->bindParam(":online_id", $player, PDO::PARAM_STR);
+$query->execute();
+$status = $query->fetchColumn();
+
 $player = htmlentities($player, ENT_QUOTES, "UTF-8");
 
-if ($scanning) {
+if ($status == 1) { // cheater
+    ?>
+    Player '<a class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover" href="/player/<?= $player; ?>"><?= $player; ?></a>' is tagged as a cheater and won't be scanned. <a class="link-underline link-underline-opacity-0 link-underline-opacity-100-hover" href="https://github.com/Ragowit/psn100/issues?q=label%3Acheater+<?= $player; ?>+OR+<?= $accountId; ?>">Dispute</a>?
+    <?php
+} elseif ($scanning) {
     ?>
     <a class='link-underline link-underline-opacity-0 link-underline-opacity-100-hover' href="/player/<?= $player; ?>"><?= $player; ?></a> is currently being scanned.
     <div class="spinner-border" role="status">
