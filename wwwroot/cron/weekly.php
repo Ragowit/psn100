d8a68dc8cec5f443c6430c1343c7d28d31261dea
Ragowit <github@grh.se>
2024-04-15 08:46:42 +0200

    Cron - Weekly Handle deadlock case...

--- wwwroot/cron/weekly.php
+++ wwwroot/cron/weekly.php
@@ -4,9 +4,18 @@ ini_set("mysql.connect_timeout", "0");
 set_time_limit(0);
 require_once("/home/psn100/public_html/init.php");
 
-$query = $database->prepare("UPDATE player
-    SET    rank_last_week = `rank`,
-           rarity_rank_last_week = rarity_rank,
-           rank_country_last_week = rank_country,
-           rarity_rank_country_last_week = rarity_rank_country ");
-$query->execute();
+do {
+    try {
+        $query = $database->prepare("UPDATE player
+            SET    rank_last_week = `rank`,
+                rarity_rank_last_week = rarity_rank,
+                rank_country_last_week = rank_country,
+                rarity_rank_country_last_week = rarity_rank_country ");
+        $query->execute();
+
+        $deadlock = false;
+    } catch (Exception $e) {
+        sleep(3);
+        $deadlock = true;
+    }
+} while ($deadlock);
