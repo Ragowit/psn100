dc8b5aa4ae218c0ab33cc9293e2b18b880d2719c
Ragowit <github@grh.se>
2025-09-03 08:26:47 +0200

    General Slugify now replace new lines with space as well

--- wwwroot/functions.php
+++ wwwroot/functions.php
@@ -3,6 +3,7 @@ function slugify($text)
 {
     $text = $text ?? "";
 
+    $text = trim(preg_replace('/\s+/', ' ', $text)); // Replace new lines with space
     $text = str_replace("&", "and", $text);
     $text = str_replace("%", "percent", $text);
     $text = str_replace(" - ", " ", $text);
