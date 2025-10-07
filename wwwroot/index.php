f474996b48aa1a196ff1342d61fb19f23b0a36ac
Markus Persson <github@grh.se>
2025-10-07 15:27:22 +0200

    Refactor application bootstrap with container

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -6,14 +6,7 @@ if ($maintenance) {
 }
 
 require_once 'init.php';
-require_once 'classes/Application.php';
-require_once 'classes/HttpRequest.php';
 
-$gameRepository = new GameRepository($database);
-$trophyRepository = new TrophyRepository($database);
-$playerRepository = new PlayerRepository($database);
-
-$router = new Router($gameRepository, $trophyRepository, $playerRepository);
-$request = HttpRequest::fromGlobals();
-$application = new Application($router, $request);
+$request = $applicationContainer->createRequestFromGlobals();
+$application = $applicationContainer->createApplication($request);
 $application->run();
