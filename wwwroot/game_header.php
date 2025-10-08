ccf0893ad53bff813a3c40f963b46dbb50255f45
Markus Persson <github@grh.se>
2025-10-08 18:40:20 +0200

    Ensure game header loads GamePlayerProgress class

--- wwwroot/game_header.php
+++ wwwroot/game_header.php
@@ -1,6 +1,8 @@
 <?php
 declare(strict_types=1);
 
+require_once __DIR__ . '/classes/Game/GamePlayerProgress.php';
+
 /** @var GameDetails $game */
 /** @var GameHeaderData $gameHeaderData */
 /** @var GamePlayerProgress|null $gamePlayer */
