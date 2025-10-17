c51eb9ae8f39a7d290a5aac3b1fae850816e63dc
Markus Persson <github@grh.se>
2025-10-17 08:27:35 +0200

    Refactor admin rescan handler into OOP processor

---
+++ wwwroot/classes/Admin/GameRescanProcessor.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../TrophyCalculator.php';
+require_once __DIR__ . '/GameRescanService.php';
+require_once __DIR__ . '/GameRescanRequestHandler.php';
+
+final class GameRescanProcessor
+{
+    private GameRescanRequestHandler $requestHandler;
+
+    public function __construct(GameRescanRequestHandler $requestHandler)
+    {
+        $this->requestHandler = $requestHandler;
+    }
+
+    public static function fromDatabase(PDO $database): self
+    {
+        $trophyCalculator = new TrophyCalculator($database);
+        $gameRescanService = new GameRescanService($database, $trophyCalculator);
+        $requestHandler = new GameRescanRequestHandler($gameRescanService);
+
+        return new self($requestHandler);
+    }
+
+    /**
+     * @param array<string, mixed> $postData
+     * @param array<string, mixed> $serverData
+     */
+    public function processRequest(array $postData, array $serverData): void
+    {
+        $this->requestHandler->handleRequest($postData, $serverData);
+    }
+}
+
