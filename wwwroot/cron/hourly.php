7e2fe72d36a1aab8aca5c0a4d87dfe457e2be3e6
Markus Persson <github@grh.se>
2025-10-18 20:48:43 +0200

    Refactor cron scripts to OOP bootstrapper

--- wwwroot/cron/hourly.php
+++ wwwroot/cron/hourly.php
@@ -2,13 +2,10 @@
 
 declare(strict_types=1);
 
-require_once dirname(__DIR__) . '/classes/Cron/CronJobRunner.php';
-require_once dirname(__DIR__) . '/classes/Cron/CronJobApplication.php';
-
-$application = CronJobApplication::create();
-$application->configureEnvironment();
-
-require_once dirname(__DIR__) . '/init.php';
+require_once dirname(__DIR__) . '/classes/Cron/CronJobBootstrapper.php';
 require_once dirname(__DIR__) . '/classes/Cron/HourlyCronJob.php';
 
-$application->run(static fn (): CronJobInterface => new HourlyCronJob($database));
+$bootstrapper = CronJobBootstrapper::create(dirname(__DIR__));
+$bootstrapper->bootstrap();
+
+$bootstrapper->run(static fn (\PDO $database): CronJobInterface => new HourlyCronJob($database));
