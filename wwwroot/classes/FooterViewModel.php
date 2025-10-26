0e927dde814e561a1ce9a438e0cf5d133479ab79
Ragowit <github@grh.se>
2025-10-26 21:39:19 +0100

    7.40

--- wwwroot/classes/FooterViewModel.php
+++ wwwroot/classes/FooterViewModel.php
@@ -51,7 +51,7 @@ final class FooterViewModel
         return new self(
             2019,
             $currentYear,
-            'v7.39',
+            'v7.40',
             'https://github.com/Ragowit/psn100/releases',
             '/changelog',
             'https://github.com/Ragowit/psn100/issues',
