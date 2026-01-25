7917b0010f7b0591c93d7c8ead92c6800bf7f8c2
Markus Persson <github@grh.se>
2026-01-26 00:41:00 +0100

    Remove extra newline in header.php

--- wwwroot/header.php
+++ wwwroot/header.php
@@ -153,7 +153,6 @@ if (isset($metaData) && $metaData instanceof PageMetaData) {
                 -webkit-mask-size: 3rem;
                 pointer-events: none;
             }
-
         </style>
 
         <script src="/js/localized-date-formatter.js" defer></script>
