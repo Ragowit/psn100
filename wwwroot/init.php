e78576ba924bc37df533c713f8deaca67ba3522d
Ragowit <github@grh.se>
2026-06-18 10:54:50 +0200

    Add cache control headers to init.php for improved caching behavior

--- wwwroot/init.php
+++ wwwroot/init.php
@@ -2,6 +2,10 @@
 
 declare(strict_types=1);
 
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 require_once __DIR__ . '/classes/ApplicationBootstrapper.php';
 
 $applicationBootstrapper = ApplicationBootstrapper::bootstrap();
